c6e0936e4403a1fd2d9d059d563aa6b27264c393
acebone11 <lolashtraylol@gmail.com>
2021-11-20 12:56:34 +0300

    лабораторная 5 вариант В

--- 3semestr/lab.py
+++ 3semestr/lab.py
@@ -5,18 +5,12 @@ a = 1
 b = 31
 k =0
 c =0
+m = int(input("число М  "))
 for h in range(a,b):
     my_list.append(h)
 print(my_list)
 
 for i in range(len(my_list)):
-    if  my_list[i] in my_list[0:i]:
+    if my_list[i] < m and not my_list[i] in my_list[0:i]:
         k+=1
 print(k)
-# с указанным вручную диапозоном
-#
-my_array =[1,2,2,3,4,4]
-for i in range(len(my_array)):
-    if my_array[i] in my_array[0:i]:
-        c+=1
-print(c)
