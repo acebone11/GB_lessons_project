8fd735209008e989e97d8184f7216581cea9b5de
acebone11 <lolashtraylol@gmail.com>
2021-11-20 10:07:44 +0300

    лабораторная 4 вариант B

---
+++ 3semestr/lab2.py
@@ -0,0 +1,33 @@
+import math
+import os
+
+
+
+
+
+def func_lab4():
+    check = True
+    last_num = 0
+    counter = 0
+
+    try:
+        while check == True:
+            num = int(input("Введите целое число, для выхода введите 0 >>>  "))
+
+            if num == 0:
+                check = False
+                break
+            digit = 3
+            if str(digit) in str(num):
+                if last_num < num:
+                    last_num = num
+                    counter += 1
+
+
+            print(f"Маскимальное число в последовательности {last_num}")
+            print("номер в последовательности  ", counter)
+
+    except ValueError:
+        print("ошибка ввода")
+
+func_lab4()
